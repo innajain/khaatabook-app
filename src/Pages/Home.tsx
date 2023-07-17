7ccc36deabd4e462354839ff4560c1dd0f8f9275
innajain <97731078+innajain@users.noreply.github.com>
2023-07-17 16:25:01 +0530

    fix: due to changed repository name

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,10 +5,10 @@ export function Home() {
     <>
     <div className="pagetitle" style={{fontFamily: "sans-serif", display:"flex", justifyContent: "center", fontSize: "4rem", marginTop: "40px"}}>Home</div>
       <div className="buttons" style={{display:"flex", justifyContent:"center", flexWrap:"wrap", gap: "10px", marginTop: "20px"}}>
-          <Button link="./vite-khaatabook-app/contacts" icon="contacts" title="Contacts" color="blue"/>
-          <Button link="./vite-khaatabook-app/your-account" icon="account_balance_wallet" title="Your Account" color="purple"/>
-          <Button link="./vite-khaatabook-app/bank-account" icon="account_balance" title="Bank Account" color="orange"/>
-          <Button link="./vite-khaatabook-app/savings" icon="currency_rupee" title="Savings" color="green"/>
+          <Button link="./khaatabook-app/contacts" icon="contacts" title="Contacts" color="blue"/>
+          <Button link="./khaatabook-app/your-account" icon="account_balance_wallet" title="Your Account" color="purple"/>
+          <Button link="./khaatabook-app/bank-account" icon="account_balance" title="Bank Account" color="orange"/>
+          <Button link="./khaatabook-app/savings" icon="currency_rupee" title="Savings" color="green"/>
       </div>
     </>
   );
